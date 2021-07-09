86e42c3f802e0023047a3f3b91db72fb30430eba
islembraham <islem.braham@esprit.tn>
2021-07-09 11:50:49 +0100

    final upload

---
+++ server.js
@@ -0,0 +1,37 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+
+
+
+const Logger = (req,res,next) => 
+{
+        let date = new Date()
+        let day = date.getDay()
+        let hour = date.getHours()
+        
+        if (hour < 9 || hour > 16 || day > 5 || day < 1) {
+                res.send('The web application is only available during working hours (Monday to Friday,  from 9 to 17)')
+        }
+                next();
+}
+app.use(Logger) 
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+app.get('/services', (req, res) => {
+  res.sendFile(path.join(__dirname + '/services.html'));
+});
+
+app.listen(5000);
+
+console.log("Running at Port 5000");
